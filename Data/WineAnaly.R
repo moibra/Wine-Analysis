9a6ec2feba184d5c977cb92e993d0b2b4b6ffd2c
Mohammed Ibraheem <md.i1995@gmail.com>
2017-07-11 23:56:13 +0530

    Update WineAnaly.R

--- Data/WineAnaly.R
+++ Data/WineAnaly.R
@@ -4,6 +4,12 @@ setwd("C:/Users/gaffar/Desktop/WineAnalysis/Wine Analy")
 getwd()
 
 library(readr)
+library(ggplot2)
+library(GGaly)
+library(plyr)
+library(gbm)
+library(caret)
+library(randomForest)
 
 #read the wine dataset
 wine<-read_csv("wine.csv")
@@ -32,25 +38,20 @@ colnames(wine)[7] <- "Flav"
 colnames(wine)[8] <- "NFlav"
 colnames(wine)[9] <- "Pro"
 colnames(wine)[12] <- "OD" 
-colnames(wine)[5]<-"Mg"
+colnames(wine)[5] <-"Mg"
 colnames(wine)[10] <- "Color"
 colnames(wine)[13] <- "Prl"
 
-#round off code (not in use here)
+#round off code (been used to get better graphs for a few decimal varying entities)
 #is.num <- sapply(wine, is.numeric)
 #wine[is.num] <- lapply(wine[is.num], round, 0)
 
-
-library(ggplot2)
-
 # finding correlation between various variables
-
-library(GGally)
 
 wine_tmp <- wine[,1:13]
 
 # Function to return points and geom_smooth
-# allow for the method to be changed
+# allow for the method to be changed: For the Loess Curve
 my_fn <- function(data, mapping, method="loess", ...){
   p <- ggplot(data = data, mapping = mapping) + 
     geom_point() + 
@@ -65,52 +66,69 @@ wine$class_num <- as.numeric(wine$class_num)
 ggpairs(wine_tmp[1:13], lower = list(continuous = my_fn))
 
 #different graphs are plotted
+
 g1 = ggplot(data = wine, aes(wine$Al)) +  
-  geom_bar(position="dodge") + facet_wrap(~class_num) 
+  geom_bar(position="dodge") + facet_wrap(~class_num) + labs(x="Alcohol Content")
 
 g2 = ggplot(data = wine, aes(wine$`Mal`)) +
-  geom_histogram(stat="count") + facet_wrap(~class_num)
+  geom_histogram(stat="count") + facet_wrap(~class_num) + labs(x="Malic Acid")
 
 g3 = ggplot(data = wine, aes(wine$Ash)) +
-  geom_histogram(stat="count") + facet_wrap(~class_num)
+  geom_histogram(stat="count") + facet_wrap(~class_num)  + labs(x="Ash")
 
 g4 = ggplot(data = wine, aes(wine$`Alc`)) +
-  geom_histogram(stat="count") + facet_wrap(~class_num)
+  geom_histogram(stat="count") + facet_wrap(~class_num)  + labs(x="Alcalinity")
 
 g5 = ggplot(data = wine, aes(wine$Mg)) +
-  geom_histogram(stat="count") + facet_wrap(~class_num)
+  geom_histogram(stat="count") + facet_wrap(~class_num)  + labs(x="Magnesium Content")
 
 g6 = ggplot(data = wine, aes(wine$`TotP`)) +
-  geom_histogram(stat="count") + facet_wrap(~class_num)
+  geom_histogram(stat="count") + facet_wrap(~class_num)  + labs(x="Total Phenols")
 
 g7 = ggplot(data = wine, aes(wine$Flav)) +
-  geom_histogram(stat="count") + facet_wrap(~class_num)
+  geom_histogram(stat="count") + facet_wrap(~class_num) + labs(x="Flavanoids")
 
 g8 = ggplot(data = wine, aes(wine$`Nflav`)) +
-  geom_histogram(stat="count") + facet_wrap(~class_num)
+  geom_histogram(stat="count") + facet_wrap(~class_num) + labs(x="Non Flavanoid Phenols")
 
 g9 = ggplot(data = wine, aes(wine$Pro)) +
-  geom_histogram(stat="count") + facet_wrap(~class_num)
+  geom_histogram(stat="count") + facet_wrap(~class_num)  + labs(x="Proanthocyanidinis")
 
 g10 = ggplot(data = wine, aes(wine$Color)) +
-  geom_histogram(stat="count") + facet_wrap(~class_num)
+  geom_histogram(stat="count") + facet_wrap(~class_num) + labs(x="Intensity of Color")
 
 g11 = ggplot(data = wine, aes(wine$Hue)) +
-  geom_histogram(stat="count") + facet_wrap(~class_num)
+  geom_histogram(stat="count") + facet_wrap(~class_num)  + labs(x="Hue")
 
 g12 = ggplot(data = wine, aes(wine$OD)) +
-  geom_histogram(stat="count") + facet_wrap(~class_num)
+  geom_histogram(stat="count") + facet_wrap(~class_num) + labs(x="OD280/OD315")
 
-g13 = ggplot(data = wine, aes(wine$Prl)) +
-  geom_histogram(stat="count") + facet_wrap(~class_num)
+#rounding off Proline values to nearest 200 to get better graph
+x<-round_any(wine$Prl,200)
+
+g13 = ggplot(data = wine, aes(x)) +
+  geom_histogram(stat="count") + facet_wrap(~class_num)  + labs(x="Proline")
 
 # 2 variables are plooted with respect to their classes
 
-k1 <- ggplot(data=wine, aes(wine$Flav,wine$TotP , color=wine$class_num)) + geom_point()
+k0 = ggplot(data = wine, aes(x,wine$OD , color= wine$class_num)) +
+  geom_point() + labs(title = "OD vs Proline" , x="Proline" , y="OD280/OD315") +
+scale_color_continuous(name="Class of wine")
 
-k2 <- ggplot(data=wine, aes(wine$OD,wine$TotP , color=wine$class_num)) + geom_point()
+k1 <- ggplot(data=wine, aes(wine$Flav,wine$TotP , color=wine$class_num)) + geom_point()  + 
+labs(title="Flav vs Tot. Phenols", x=" Flavanoids", 
+                      y = "Total Phenols") + 
+  scale_color_discrete(name="Class of Wine")
+
+k2 <- ggplot(data=wine, aes(wine$OD,wine$TotP , color=wine$class_num)) + geom_point()  
++ labs(title="OD vs Tot. Phenols", x="OD218/315", 
+                      y = "Total Phenols") + 
+  scale_color_discrete(name="Class of Wine")
 
 k3 <- ggplot(data=wine, aes(wine$OD,wine$Flav , color=wine$class_num)) + geom_point()
+ + labs(title="OD vs Flavanoids", x=" OD ", 
+                      y = "Flavanoids") + 
+  scale_color_discrete(name="Class of Wine")
 
 #factorise class_num to begin modeling
 wine$class_num <- as.factor(wine$class_num)
@@ -124,11 +142,8 @@ split <- round(nrow(wine) * .70)
 train <- wine[1:split,]
 test <- wine[(split + 1):nrow(wine),]
 
-library(gbm)
 
 #gbmpackage is being used because there are 3 variables in the Class column
-
-library(caret)
 
 set.seed(20)
 
@@ -148,6 +163,9 @@ summary(model, order=TRUE)
 
 Predict <- predict(model, test)
 
+#table to compute Accuracy
+table(test$class_num,Predict)
+
 #combining the columns
 
 output <- cbind(test, Predict)
@@ -159,7 +177,6 @@ conf <- table(test$class_num, Predict)
 acc <- sum(diag(conf))/sum(conf)
 acc
 
-library(randomForest)
 
 # define the control using a random forest selection function
 
@@ -203,5 +220,3 @@ predict2 <- predict(model2, test)
 conf <- table(test$class_num, predict2)
 acc2 <- sum(diag(conf))/sum(conf)
 acc2
-
-
